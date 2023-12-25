87384bdc827202bda9921911550176909dc41692
Oles Malysh <olesmalish37@gmail.com>
2023-12-25 17:55:13 +0100

    Finally done fragment_isnide_animal with ScrollView

--- app/src/main/java/com/example/animalhandbook/Fragments/AnimalListFragment.kt
+++ app/src/main/java/com/example/animalhandbook/Fragments/AnimalListFragment.kt
@@ -1,18 +1,19 @@
-package com.example.animalhandbook
+package com.example.animalhandbook.Fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.animalhandbook.R
 
-class FragmentTypeAnimal : Fragment() {
+class AnimalListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_type_animal, container, false)
+        return inflater.inflate(R.layout.fragment_animal_list, container, false)
     }
 
 
--- app/src/main/java/com/example/animalhandbook/Fragments/WelcomeFragment.kt
+++ app/src/main/java/com/example/animalhandbook/Fragments/WelcomeFragment.kt
@@ -1,10 +1,12 @@
-package com.example.animalhandbook
+package com.example.animalhandbook.Fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.animalhandbook.databinding.FragmentWelcomeBinding
 
 
@@ -14,5 +16,14 @@ class WelcomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentWelcomeBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.testBtn.setOnClickListener {
+            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToInsideAnimalFragment())
+        }
+
+
     }
 }
