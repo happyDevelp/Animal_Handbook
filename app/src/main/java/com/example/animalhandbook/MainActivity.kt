797d83ae02fd3f0bf4525d56c518d274b58d8eac
Oles Malysh <olesmalish37@gmail.com>
2024-01-07 20:10:12 +0100

    on dev branch try to implement work-correct menu

--- app/src/main/java/com/example/animalhandbook/MainActivity.kt
+++ app/src/main/java/com/example/animalhandbook/MainActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import androidx.core.app.TaskStackBuilder
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.animalhandbook.Fragments.AnimalListFragment
 import com.example.animalhandbook.Fragments.InsideAnimalFragment
 import com.example.animalhandbook.Fragments.Tab.FavouritesFragment
@@ -19,27 +24,35 @@ import java.nio.file.attribute.FileAttributeView
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.navView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.navigation_favourites -> replace(FavouritesFragment())
-                R.id.navigation_home -> replace(WelcomeFragment())
-                R.id.navigation_settings -> replace(SettingsFragment())
-            }
-            true
-        }
+        drawerLayout = binding.drawerLayout
+
+        val appBarConfiguration = AppBarConfiguration(R.id.navigation_favourites, R.id.navigation_settings)
+
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
 
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
+
     private fun replace(fragment: Fragment) {
-            val fragmentManager = supportFragmentManager
-            val fragmentTransaction = fragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
-            fragmentTransaction.commit()
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
+        fragmentTransaction.commit()
 
     }
 
