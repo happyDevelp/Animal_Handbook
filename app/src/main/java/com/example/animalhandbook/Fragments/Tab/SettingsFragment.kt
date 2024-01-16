556408f2585f87090e90ed2be716c0cba102fbe6
Oles Malysh <olesmalish37@gmail.com>
2024-01-16 11:14:11 +0100

    Settings fragment is done. In future will rework it adding more functionality

--- app/src/main/java/com/example/animalhandbook/Fragments/Tab/SettingsFragment.kt
+++ app/src/main/java/com/example/animalhandbook/Fragments/Tab/SettingsFragment.kt
@@ -1,19 +1,43 @@
 package com.example.animalhandbook.Fragments.Tab
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.animalhandbook.R
+import com.example.animalhandbook.databinding.FragmentSettingsBinding
+
 class SettingsFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    private lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentSettingsBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.instImageView.setOnClickListener {
+            openUrl("https://www.instagram.com/valtornist?igsh=MXVucnV6ODB4YXFxNw==")
+        }
+
+        binding.linkedInImageView.setOnClickListener {
+            openUrl("https://www.linkedin.com/in/oles-malysh-3bb888257/")
+        }
+
+        binding.gitHubImageView.setOnClickListener {
+            openUrl("https://github.com/happyDevelp")
+        }
+    }
+
+    private fun openUrl(url: String) {
+        val uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+
+        startActivity(intent)
+    }
 
 }
